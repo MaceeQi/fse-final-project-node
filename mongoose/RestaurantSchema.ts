b8341ff11df24eb72a7feb9e3545733f66b67d59
nala-he <hytdaisy@gmail.com>
2022-11-21 22:33:26 -0500

    added PostSchema and PostModel

--- mongoose/RestaurantSchema.ts
+++ mongoose/RestaurantSchema.ts
@@ -2,10 +2,21 @@
  * @file Implements mongoose schema for restaurants
  */
 import mongoose from "mongoose";
-import Hour from "../models/Hour";
-import Post from "../models/Post";
-import FeaturedItem from "../models/FeaturedItem";
 
+/**
+ * @typedef {Restaurant} Restaurant Represents a restaurant object
+ * @property {string} name Restaurant name
+ * @property {string} bannerPicture Banner picture
+ * @property {string} profilePicture Avatar picture
+ * @property {string} handle Restaurant account handle
+ * @property {string} bio Restaurant bio
+ * @property {string} cuisine Restaurant cuisine type
+ * @property {string} price Restaurant price range
+ * @property {string} address Restaurant address
+ * @property {string} hours Restaurant open hours
+ * @property {string} phone Restaurant phone number
+ * @property {string} website Restaurant website
+ */
 const RestaurantSchema = new mongoose.Schema({
     name: {type: String, required: true},
     bannerPicture: String,
